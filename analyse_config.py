3acf09bfa20387c1ab42d956fd0960210dc6fb26
lijiankai <lijiankai@dmzj.com>
2018-01-08 13:42:16 +0800

    analyse_config.py 微调

--- analyse_config.py
+++ analyse_config.py
@@ -1,4 +1,3 @@
-# ---------- 自定义部分 ----------#
 # ----日志格式
 # 利用非贪婪匹配和分组匹配, 需要严格参照日志定义中的分隔符和引号
 log_pattern = r'^(?P<remote_addr>.*?) - \[(?P<time_local>.*?) \+0800\] "(?P<request>.*?)"' \
@@ -21,10 +20,9 @@ mongo_host = '172.16.2.24'
 mongo_port = 27017
 # 存储设置
 # mongodb存储结构为每个站点对应一个库, 每天对应一个集合, 每分钟每server产生一个统计结果文档(即分析粒度最小达到分钟级)
-# 为了使mongodb数据集尽量小, 每分钟统计结果中, 只取点击数前MAX_URI_NUM的uri, 每个uri中点击数前MAX_ARG_NUM的args 进行入库
+# 为了使mongodb数据集尽量小, 每分钟统计结果中, 只取点击数前MAX_URI_NUM的uri, 每个uri中点击数前MAX_ARG_NUM的args进行入库
 MAX_URI_NUM = 100
 MAX_ARG_NUM = 20
 
 # 保存几天的数据(每天一个集合)
 LIMIT = 10
-# ---------- 自定义部分结束 ----------#
